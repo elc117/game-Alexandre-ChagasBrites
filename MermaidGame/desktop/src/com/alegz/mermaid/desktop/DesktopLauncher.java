e0dd0c036dcb4216c559036e5a6c40af2370fa9b
alexa <alexa@LAPTOP-P2QBA60R>
2020-07-23 20:56:15 -0300

    Upload inicial do projeto

---
+++ MermaidGame/desktop/src/com/alegz/mermaid/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.alegz.mermaid.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.alegz.mermaid.MermaidGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MermaidGame(), config);
+	}
+}
