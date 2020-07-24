5dce9a23f016448188e3a5f3909bcf46024708e9
Alexandre Chagas Brites <alexandrechagasbrites@gmail.com>
2020-07-23 21:41:42 -0300

    Removendo arquivos

--- mermaid-game-core/src/com/alegz/mermaid/physics/Collider.java
+++
@@ -1,9 +0,0 @@
-package com.alegz.mermaid.physics;
-
-import com.badlogic.gdx.physics.box2d.Body;
-import com.badlogic.gdx.physics.box2d.Fixture;
-
-public interface Collider 
-{
-	public Fixture getFixture(Body body);
-}
--- mermaid-game-core/src/com/alegz/mermaid/rendering/material/MaterialAttribute.java
+++
@@ -1,9 +0,0 @@
-package com.alegz.mermaid.rendering.material;
-
-import com.badlogic.gdx.graphics.glutils.ShaderProgram;
-
-public abstract class MaterialAttribute 
-{
-	public String key;
-	public abstract void set(ShaderProgram shader);
-}
